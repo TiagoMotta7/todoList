fd520fc3fce1a52f43d11d0cbd5f962bdac0c959
Tiago Motta <tiago91@gmail.com>
2021-09-23 13:38:44 -0300

    Criação de formulário

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = React.useState([])
+  const [todo, setTodo] = React.useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit}>
+        <input type="text" onchange={(e) => setTodo(e.target.value)} value={todo}/>
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   );
 }
