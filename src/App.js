3ebf69546bd73fa8af2a98ba7a392c886280924a
Tiago Motta <tiago91@gmail.com>
2021-09-23 14:32:55 -0300

    Código antes de criar o localstorage

--- src/App.js
+++ src/App.js
@@ -4,17 +4,83 @@ import './App.css';
 function App() {
   const [todos, setTodos] = React.useState([])
   const [todo, setTodo] = React.useState("")
+  const [todoEditing, setTodoEditing] = React.useState(null)
+  const [editingText, setEditingText] = React.useState("")
 
   function handleSubmit(e) {
     e.preventDefault()
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed:false,
+    }
+
+    setTodos([...todos].concat(newTodo))
+    setTodo("")
   }
+
+  function deleteTodo(id){
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id)
+
+    setTodos(updatedTodos)
+  }
+
+  function toggleComplete(id){
+    const updatedTodos = [...todos].map((todo) => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+
+  function editTodo(id){
+    const updatedTodos =[...todos].map((todo) => {
+      if(todo.id === id){
+        todo.text = editingText;
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+    setTodoEditing(null)
+    setEditingText("")
+  }
+
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="text" onchange={(e) => setTodo(e.target.value)} value={todo}/>
+        <input type="text"  onChange={(e) => setTodo(e.target.value)} value={todo}/>
         <button type="submit">Add Todo</button>
       </form>
+      {todos.map((todo) => <div key={todo.id}>
+
+        {todoEditing === todo.id ? (<input 
+          type="text" 
+          onChange={(e) => setEditingText(e.target.value)} 
+          value={editingText}
+        />) 
+          : 
+          (<div>{todo.text}</div>)}       
+        
+
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        <input 
+          type="checkbox" 
+          onChange={() => toggleComplete(todo.id)} 
+          checked={todo.completed}/>
+
+          {todoEditing === todo.id ? (<button onClick={() => editTodo(todo.id)}>Submit Edits</button>) 
+          : (<button onClick={() => setTodoEditing(todo.id)}>Edit Todo</button>)}
+
+          
+          
+        </div>)}      
     </div>
   );
 }
